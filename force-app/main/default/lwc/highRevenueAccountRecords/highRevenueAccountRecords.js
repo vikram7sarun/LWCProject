c3a776b9c6ced022fff3feecd88b17d05251a3f0
vikramc <vikram7sarun@gmail.com>
2025-03-04 19:02:51 -0500

    HighRevenueRecords

--- force-app/main/default/lwc/highRevenueAccountRecords/highRevenueAccountRecords.js
+++ force-app/main/default/lwc/highRevenueAccountRecords/highRevenueAccountRecords.js
@@ -2,6 +2,14 @@ import { LightningElement, wire } from 'lwc';
 import HIGH_REVENUE_ACCOUNT from '@salesforce/apex/AccountController.getHighRevenueAccountRecords';
 
 export default class HighRevenueAccountRecords extends LightningElement {
+    accountToDisplay = [];
 
-
+    @wire(HIGH_REVENUE_ACCOUNT)
+    wiredAccounts({ data, error }) {
+        if (data) {
+            this.accountToDisplay = [...data]; // Immutable update
+        } else if (error) {
+            console.error('Error fetching high revenue accounts:', error);
+        }
+    }
 }
